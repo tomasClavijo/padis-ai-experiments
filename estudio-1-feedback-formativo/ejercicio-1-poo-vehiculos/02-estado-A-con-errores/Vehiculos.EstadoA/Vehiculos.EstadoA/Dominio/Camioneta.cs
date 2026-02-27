8e70906314a76ef079dcb4effebe537ad696392d
tomasClavijo <tmsclvj410@gmail.com>
2026-02-27 12:30:59 -0300

    feat: estado A baseline vehiculos (solucion estudiantil con fallas POO)

---
+++ estudio-1-feedback-formativo/ejercicio-1-poo-vehiculos/02-estado-A-con-errores/Vehiculos.EstadoA/Vehiculos.EstadoA/Dominio/Camioneta.cs
@@ -0,0 +1,14 @@
+﻿namespace Vehiculos.EstadoA.Dominio;
+
+public class Camioneta : Vehiculo
+{
+    public Camioneta(int cantidadPuertas, string colorChasis)
+        : base(cantidadPuertas, colorChasis)
+    {
+    }
+
+    public new void Encender()
+    {
+        Console.WriteLine($"Encendiendo vehículo con cantidad de puertas {cantidadPuertas} y color de chasis {colorChasis}");
+    }
+}
---
+++ estudio-1-feedback-formativo/ejercicio-1-poo-vehiculos/02-estado-A-con-errores/Vehiculos.EstadoA/Vehiculos.EstadoA/Program.cs
@@ -0,0 +1,25 @@
+﻿using Vehiculos.EstadoA.Dominio;;
+
+var vehiculos = new List<Vehiculo>();
+
+vehiculos.Add(new Auto(4, "Rojo"));
+vehiculos.Add(new Camioneta(2, "Negro"));
+
+foreach (var v in vehiculos)
+{
+    v.Encender();
+}
+
+Console.WriteLine("----");
+
+foreach (var v in vehiculos)
+{
+    if (v is Auto a)
+    {
+        a.Encender("extra");
+    }
+    else if (v is Camioneta c)
+    {
+        c.Encender();
+    }
+}
